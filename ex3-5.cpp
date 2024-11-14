f3099490d0622afcf9b781a0b74b7de8cc4348ed
luckydogIsaac <sp1131215@spjh.tp.edu.tw>
2024-11-14 19:21:38 +0800

    Create ex3-5.cpp

---
+++ ex3-5.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y;
+
+    printf("please input int x=");
+    scanf("%d",&x);
+    printf("please input y=");
+    scanf("%d",&y);
+    printf("x+y=%d\n",x+y);
+    printf("x-y=%d\n",x-y);
+    printf("x*y=%d\n",x*y);
+    printf("x/y=%d\n",x/y);
+    printf("x mod y=%d\n",x%y);
+    return 0;
+}
