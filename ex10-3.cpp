435fad84bb66e149dcc726addc3c1f360bbf6038
luckydogIsaac <sp1131215@spjh.tp.edu.tw>
2024-12-26 19:42:13 +0800

    Create ex10-3.cpp

---
+++ ex10-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int myAge = 43;
+
+    printf("myAge value = %d\n", myAge);
+    printf("myAge's address in memory = %p", &myAge);//Ourputs 07ffe5367e044
+
+
+    return 0;
+}
